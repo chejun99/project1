410ee15fbf9ba08532f7d22ea10f90cfa461085d
Limchejun <chejun99@gmail.com>
2017-02-05 12:55:23 +0900

    sensor -> project 명령어실수 해결

--- pySerial/python_to_DB.py
+++ pySerial/python_to_DB.py
@@ -6,13 +6,14 @@ ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 conn = MySQLdb.connect(host='localhost',user='root',passwd='wns1077511',db='test')
-
+# print("error8")
 cur=conn.cursor()
-
-sql="insert into sensor(sign,time) values (%s,%s)"
-
+# print("error10")
+sql="insert into project(sign,time) values (%s,%s)"
+# print("error12")
 container=["0",st]
-
+# print("error14")
 cur.execute(sql, container)
-
+# print("error16")
 conn.commit()
+# print("error18")
