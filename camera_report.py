86bf115cc5ede3a51abc97d58b05654a54e94d05
Sunghyun-Lee <sunghyun@dable.io>
2017-05-09 14:14:02 +0900

    change on camera_report

--- camera_report.py
+++ camera_report.py
@@ -3,8 +3,14 @@ import io
 import random
 import picamera
 from PIL import Image
+import time
 
 prior_image = None
+threshold = 10  # How Much pixel changes
+sensitivity = 100  # How many pixels change
+height = 1640
+width = 1232
+
 
 def detect_motion(camera):
     global prior_image
@@ -21,6 +27,39 @@ def detect_motion(camera):
         result = random.randint(0, 10) == 0
         prior_image = current_image
         return result
+
+def scanMotion(width, height):
+    global sensitivity
+    motionFound = False
+    data1 = takeMotionImage(width, height)
+    while not motionFound:
+        data2 = takeMotionImage(width, height)
+        diffCount = 0L
+        for w in range(0, width):
+            for h in range(0, height):
+                # get the diff of the pixel. Conversion to int
+                # is required to avoid unsigned short overflow.
+                diff = abs(int(data1[h][w][1]) - int(data2[h][w][1]))
+                if  diff > threshold:
+                    diffCount += 1
+            if diffCount > sensitivity:
+                break; #break outer loop.
+        if diffCount > sensitivity:
+            motionFound = True
+        else:
+            data2 = data1              
+    return motionFound
+
+
+def takeMotionImage(width, height):
+    with picamera.PiCamera() as camera:
+        time.sleep(1)
+        camera.resolution = (width, height)
+        with picamera.array.PiRGBArray(camera) as stream:
+            camera.exposure_mode = 'auto'
+            camera.awb_mode = 'auto' 
+            camera.capture(stream, format='rgb')
+            return stream.array
 
 def write_video(stream):
     with io.open('before.h264', 'wb') as output:
