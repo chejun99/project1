edeb1cee2b051e26f232bce5e1a6aa1add6a9173
chejun <chejun99@gmail.com>
2017-05-09 05:29:44 +0000

    새로운 체크 코드로 변경

--- camera_report.py
+++ camera_report.py
@@ -12,21 +12,21 @@ height = 1640
 width = 1232
 
 
-def detect_motion(camera):
-    global prior_image
-    stream = io.BytesIO()
-    camera.capture(stream, format='jpeg', use_video_port=True)
-    stream.seek(0)
-    if prior_image is None:
-        prior_image = Image.open(stream)
-        return False
-    else:
-        current_image = Image.open(stream)
-        if prior_image == current_iamge:
-            print('sucess')
-        result = random.randint(0, 10) == 0
-        prior_image = current_image
-        return result
+# def detect_motion(camera):
+#     global prior_image
+#     stream = io.BytesIO()
+#     camera.capture(stream, format='jpeg', use_video_port=True)
+#     stream.seek(0)
+#     if prior_image is None:
+#         prior_image = Image.open(stream)
+#         return False
+#     else:
+#         current_image = Image.open(stream)
+#         if prior_image == current_iamge:
+#             print('sucess')
+#         result = random.randint(0, 10) == 0
+#         prior_image = current_image
+#         return result
 
 def scanMotion(width, height):
     global sensitivity
@@ -47,7 +47,7 @@ def scanMotion(width, height):
         if diffCount > sensitivity:
             motionFound = True
         else:
-            data2 = data1              
+            data2 = data1
     return motionFound
 
 
@@ -57,38 +57,38 @@ def takeMotionImage(width, height):
         camera.resolution = (width, height)
         with picamera.array.PiRGBArray(camera) as stream:
             camera.exposure_mode = 'auto'
-            camera.awb_mode = 'auto' 
+            camera.awb_mode = 'auto'
             camera.capture(stream, format='rgb')
             return stream.array
 
-def write_video(stream):
-    with io.open('before.h264', 'wb') as output:
-        for frame in stream.frames:
-            if frame.header:
-                stream.seek(frame.position)
-                break
-        while True:
-            buf = stream.read1()
-            if not buf:
-                break
-            output.write(buf)
-    stream.seek(0)
-    stream.truncate()
+# def write_video(stream):
+#     with io.open('before.h264', 'wb') as output:
+#         for frame in stream.frames:
+#             if frame.header:
+#                 stream.seek(frame.position)
+#                 break
+#         while True:
+#             buf = stream.read1()
+#             if not buf:
+#                 break
+#             output.write(buf)
+#     stream.seek(0)
+#     stream.truncate()
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (1280, 720)
-    stream = picamera.PiCameraCircularIO(camera, seconds=10)
-    camera.start_recording(stream, format='h264')
-    try:
-        while True:
-            camera.wait_recording(1)
-            if detect_motion(camera):
-                print('Motion detected!')
-                camera.split_recording('after.h264')
-                write_video(stream)
-                while detect_motion(camera):
-                    camera.wait_recording(1)
-                print('Motion stopped!')
-                camera.split_recording(stream)
-    finally:
-        camera.stop_recording()
+# with picamera.PiCamera() as camera:
+#     camera.resolution = (1280, 720)
+#     stream = picamera.PiCameraCircularIO(camera, seconds=10)
+#     camera.start_recording(stream, format='h264')
+#     try:
+#         while True:
+#             camera.wait_recording(1)
+#             if detect_motion(camera):
+#                 print('Motion detected!')
+#                 camera.split_recording('after.h264')
+#                 write_video(stream)
+#                 while detect_motion(camera):
+#                     camera.wait_recording(1)
+#                 print('Motion stopped!')
+#                 camera.split_recording(stream)
+#     finally:
+#         camera.stop_recording()
